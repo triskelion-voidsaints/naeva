0436a193c0ad3b30bd95fa441643b6cd29a915dc
RavelinW <githubaccount@ravelin.addy.io>
2025-04-10 22:45:58 -0500

    Hexaporya Hypergates

---
+++ spob/lua/hypergate_p777.lua
@@ -0,0 +1,22 @@
+local hypergate = require "spob.lua.lib.hypergate"
+hypergate.setup{
+   basecol = { 0.172, 0.458, 1 },
+   cost_mod = {
+      [100] = 0,
+      [90]  = 0.1,
+      [80]  = 0.2,
+      [70]  = 0.3,
+      [50]  = 0.5,
+      [30]  = 0.8,
+      [10]  = 0.9,
+   },
+   tex = "hypergate_zalek_activated.webp",
+}
+
+hypergate.setup{
+   destfunc = function ()
+      return {
+         spob.get("HG-Residio"),
+      }
+   end,
+}
