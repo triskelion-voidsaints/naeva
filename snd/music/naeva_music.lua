fb529d8f04a888f170009e9d72b040ee9bec9085
RavelinW <githubaccount@ravelin.addy.io>
2025-06-24 23:31:14 -0500

    Add and update spob tags and dynamic music selection
    Introduces new tags (e.g., ruined, abandoned, pirate, prison, mysterious, terrorist, amazing) to many spob XML files for improved categorization. Updates naeva_music.lua to use a dynamic function for spob-specific music selection based on tags, class, and services, replacing the static spob_songs table. Also standardizes price_mod formatting and population notation in several spob files.

--- snd/music/naeva_music.lua
+++ snd/music/naeva_music.lua
@@ -1,73 +1,91 @@
 return {
+   -- Priority of the songs here. Lower priority will precede. Defaults to 5 if unspecified.
+   priority = 9,
    loading_songs = {
       "mars.ogg",
    },
    intro_songs = {
       "mars.ogg",
    },
-   credits_songs = {
-      "empire1.ogg",
-   },
-   -- Songs chosen randomly when taking off
-   takeoff_songs = {
-      "liftoff.ogg",
-      "launch2.ogg",
-      "launch3chatstart.ogg",
-   },
-   -- Neutral ambient songs
-   ambient_songs = {
-      "ambient2.ogg", "mission.ogg",
-      "peace1.ogg", "peace2.ogg", "peace4.ogg", "peace6.ogg",
-      "void_sensor.ogg", "ambiphonic.ogg",
-      "ambient4.ogg", "terminal.ogg", "eureka.ogg",
-      "ambient2_5.ogg", "78pulse.ogg", "therewillbestars.ogg",
-   },
-   -- Factional songs. Systems default to songs of dominant factions.
-   factional_songs = {
-      Federation = { "zalek2.ogg", "empire1.ogg", add_neutral = true },
-	  ["Aurora Empire"] = { "dvaered1.ogg", "dvaered2.ogg", add_neutral = true },
-   },
-   nebula_combat_songs = {
-      "nebu_battle1.ogg",
-      "nebu_battle2.ogg",
-      "combat1.ogg",
-      "combat2.ogg",
-   },
-   combat_songs = {
-      "combat3.ogg",
-      "combat1.ogg",
-      "combat2.ogg",
-      "vendetta.ogg",
-   },
-   -- Factional combat songs. Defaults to dominant factions.
-   factional_combat_songs = {
---	  Federation = { "battlesomething2.ogg" },
---	  Polaris = { "battlesomething2.ogg" },
---	  Empire = { "battlesomething2.ogg" },
-      Pirate     = { "battlesomething2.ogg", "blackmoor_tides.ogg", add_neutral = true },
-   },
    -- Spob-specific songs. Replace songs for certain spobs
    spob_songs = {
-	  ["Kolan"] = { "agriculture.ogg" },
-	  ["LPAD"] = { "intrigue.ogg" },
-	  ["Goliath"] = { "ambient3.ogg" },
-	  ["Kyractus"] = { "tension.ogg" },
-	  ["Hax"] = { "intrigue.ogg" },
-	  ["Selon"] = { "tension.ogg" },
-	  ["Vyndycalus"] = { "intrigue.ogg" },
 	  ["Ventus"] = { "dark_city.ogg" },
-	  ["MDS-01"] = { "ambient3.ogg" },
 	  ["Drift"] = { "imminent_threat.ogg" },
-	  ["UHP-0474"] = { "intrigue.ogg" },
-	  ["Lupus"] = { "tension.ogg" },
-	  ["Irradiated Deimos"] = { "intrigue.ogg" },
-	  ["Llyrithra"] = { "tension.ogg" },
-	  ["Vellos Prime"] = { "tension.ogg" },
-	  ["Korell Prime"] = { "intrigue.ogg" },
-	  ["Harbor"] = { "pirate1_theme1.ogg" },
-	  ["Hrunyir's Exchange"] = { "pirate1_theme1.ogg" },
-	  ["Houseless"] = { "pirate1_theme1.ogg" },
    },
+   -- Spob-specific songs.
+   spob_songs_func = function( spb )
+      local class = spb:class()
+      local tags = spb:tags()
+      local services = spb:services()
+
+      -- Special conditions that limit
+      if tags.ruined then
+         return {
+            "ruined_suspense.ogg",
+            "dark_orchestra.ogg",
+            "space_emergency.ogg",
+         }
+      elseif tags.abandoned then
+         return {
+            "nordic_saxo.ogg",
+         }
+      elseif tags.destroyed then
+         return {
+            "intrigue.ogg",
+            "tension.ogg",
+         }
+      elseif tags.mysterious then
+         return {
+            "end_of_time.ogg",
+            "the_last_mystery.ogg",
+            "winterstorm_1.ogg"
+         }
+      elseif tags.pirate then
+         return {
+            "pirate1_theme1.ogg",
+         }
+      end
+
+      -- We'll add stuff here
+      local lst = {}
+      if tags.urban then
+         tmergei( lst, { "meet_the_fish.ogg" } )
+      end
+      if tags.pirate then
+         tmergei( lst, { "pirate1_theme1.ogg" } )
+      end
+      if tags.station then
+         tmergei( lst, { "cosmostation.ogg", "snabba_labba.ogg" } )
+      end
+      if services.shipyard then
+         tmergei( lst, { "gonna_be_gone.ogg" } )
+      end
+
+      -- Added based on class
+      if class == "M" or class == "MD" or class=="H" then
+         tmergei( lst, { "agriculture.ogg", "peaceful_world.ogg", "boschs_garden.ogg" } )
+      elseif class == "O" then
+         tmergei( lst, { "ocean.ogg" } )
+      elseif class == "P" then
+         tmergei( lst, { "snow.ogg", "nordic_winter_25_for_25.ogg" } )
+      elseif class=="I" or class=="J" or class=="S" or class=="T" or class=="Y" then
+         if not services.inhabited then
+            tmergei( lst, { "methyl_swamp.ogg" } )
+         end
+      end
+
+      -- Nothing add, so try to add something
+      if #lst <= 0 then
+         -- More generic defaults
+         if services.inhabited then
+            tmergei( lst, { "upbeat.ogg" } )
+         else
+            tmergei( lst, { "end_of_time.ogg", "the_last_mystery.ogg", "winterstorm_1.ogg" } )
+         end
+      end
+
+      return lst
+   end,
    -- System-specific songs. Replace songs for certain systems
    system_ambient_songs = {
 	  ["Ventyy"] = { "/snd/music/dark_city.ogg" },
